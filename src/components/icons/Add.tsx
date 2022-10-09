d852253d1083e6e581162288cc9ebf1d6fd8ad3d
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-09 16:05:13 +0200

    extracted add-icon into separate component

---
+++ src/components/icons/Add.tsx
@@ -0,0 +1,16 @@
+export const Add = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={3}
+    stroke="currentColor"
+    className="h-4 w-4"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>
+);
