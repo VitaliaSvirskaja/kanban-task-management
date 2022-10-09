c376d6b65e2793972c0c77d5423e7a018e38ffde
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-09 11:17:23 +0200

    init theming (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ export const App = () => {
 
   return (
     <div className="flex h-screen flex-col">
-      <header className="h-20 bg-blue-300">Header</header>
+      <header className="flex h-20">
+        <div className="w-80 border-r border-r-lines-light">Logo</div>
+        <div>Platform Launch</div>
+      </header>
       <div className="relative flex h-full">
         <aside
-          className={`absolute top-0 left-0 h-full w-80 bg-amber-300 transition-transform ${
+          className={`absolute top-0 left-0 h-full w-80 border-r border-r-lines-light transition-transform ${
             isSidebarVisible ? "" : "-translate-x-full"
           }`}
         >
           <div>Sidebar</div>
         </aside>
         <main
-          className={`w-full bg-violet-300 transition-spacing ${
+          className={`w-full bg-light-grey transition-spacing ${
             isSidebarVisible ? "ml-80" : ""
           }`}
           onClick={() => setIsSidebarVisible(!isSidebarVisible)}
