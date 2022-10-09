121e1340e2cd616590331324f80a19d08703570a
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-09 18:17:43 +0200

    implementing logic for sidebar (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Header } from "./components/Header";
+import { boardContext } from "./context/BoardContext";
 
 export const App = () => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const { boards, selectBoard, activeBoard } = useContext(boardContext);
 
   return (
     <div className="flex h-screen flex-col">
@@ -14,7 +16,22 @@ export const App = () => {
             isSidebarVisible ? "" : "-translate-x-full"
           }`}
         >
-          <div>Sidebar</div>
+          <div className="pl-8">All boards {boards.length}</div>
+          <div>
+            {boards.map((board) => {
+              return (
+                <li
+                  key={board.id}
+                  className={`mr-10 list-none pl-8 ${
+                    activeBoard === board.id && "bg-primary"
+                  }`}
+                  onClick={() => selectBoard(board.id)}
+                >
+                  {board.title}
+                </li>
+              );
+            })}
+          </div>
         </aside>
         <main
           className={`w-full bg-light-grey transition-spacing ${
