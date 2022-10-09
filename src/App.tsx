d7f3eb9fe32ab9504fe2468bfc380e01584c762f
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-09 10:55:06 +0200

    implementing basic layout (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
-export const App = () => (
-  <div className="text-center">Tailwind is working!</div>
-);
+import { useState } from "react";
+
+export const App = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  return (
+    <div className="flex h-screen flex-col">
+      <header className="h-20 bg-blue-300">Header</header>
+      <div className="relative flex h-full">
+        <aside
+          className={`absolute top-0 left-0 h-full w-80 bg-amber-300 transition-transform ${
+            isSidebarVisible ? "" : "-translate-x-full"
+          }`}
+        >
+          <div>Sidebar</div>
+        </aside>
+        <main
+          className={`w-full bg-violet-300 transition-spacing ${
+            isSidebarVisible ? "ml-80" : ""
+          }`}
+          onClick={() => setIsSidebarVisible(!isSidebarVisible)}
+        >
+          Content
+        </main>
+      </div>
+    </div>
+  );
+};
