1d42a8bc3bee3c05de7a28bcf24437890709a608
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-11 23:29:30 +0200

    implemented close icon

---
+++ src/components/icons/Close.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+export const Close = ({ className, ...svgProps }: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={`h-5 w-5 text-medium-grey hover:cursor-pointer hover:text-primary ${className}`}
+    {...svgProps}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+);
