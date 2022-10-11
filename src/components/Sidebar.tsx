96122383b18a176caa9861f2217fa4e30f8ae467
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-11 08:46:23 +0200

    implemented theme toggle without logic (wip)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { boardContext } from "../context/BoardContext";
 import { Board } from "./icons/Board";
 import { Listelement } from "./Listelement";
 import { Add } from "./icons/Add";
 import { HideSidebar } from "./icons/HideSidebar";
+import { Switch } from "@headlessui/react";
+import { DarkTheme } from "./icons/DarkTheme";
+import { LightTheme } from "./icons/LightTheme";
 
 interface Props {
   isSidebarVisible: boolean;
@@ -12,6 +15,7 @@ interface Props {
 
 export const Sidebar = ({ isSidebarVisible, onClose }: Props) => {
   const { boards, selectBoard, activeBoard } = useContext(boardContext);
+  const [isThemeDark, setIsThemeDark] = useState(false);
 
   return (
     <aside
@@ -43,13 +47,34 @@ export const Sidebar = ({ isSidebarVisible, onClose }: Props) => {
             className="text-primary"
           />
         </div>
+        <div>
+          {/* TODO Als Komponente auslagern und Logik implementieren*/}
+          <div
+            className="ml-6 flex items-center justify-center gap-6
+           rounded-lg bg-light-grey py-4"
+          >
+            <LightTheme />
+            <Switch
+              checked={isThemeDark}
+              onChange={setIsThemeDark}
+              className={`relative inline-flex h-6 w-11 items-center rounded-full bg-primary hover:bg-primary-light`}
+            >
+              <span
+                className={`${
+                  isThemeDark ? "translate-x-6" : "translate-x-1"
+                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+              />
+            </Switch>
+            <DarkTheme />
+          </div>
 
-        <Listelement
-          icon={<HideSidebar />}
-          text="Hide Sidebar"
-          className="mb-6 text-medium-grey hover:text-primary"
-          onClick={onClose}
-        />
+          <Listelement
+            icon={<HideSidebar />}
+            text="Hide Sidebar"
+            className="mb-6 text-medium-grey hover:text-primary"
+            onClick={onClose}
+          />
+        </div>
       </div>
     </aside>
   );
