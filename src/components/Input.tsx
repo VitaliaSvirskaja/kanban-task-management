e00c58c54bb43d57873efdda4a9929e3f720715c
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-12 20:03:36 +0200

    fixed issue with forwardref & eslint

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,12 @@
-import React, { InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes } from "react";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
   error?: string;
-  variant?: "filled";
 }
 
-// eslint-disable-next-line react/display-name
-export const Input = React.forwardRef<HTMLInputElement, Props>(
-  ({ error, label, variant, ...inputProps }, ref) => (
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ error, label, ...inputProps }, ref) => (
     <div className="flex w-full flex-col gap-1">
       <label htmlFor={inputProps.name} className="body-m text-medium-grey">
         {label}
@@ -17,14 +15,13 @@ export const Input = React.forwardRef<HTMLInputElement, Props>(
         placeholder="e.g. Web Design"
         ref={ref}
         {...inputProps}
-        className={`rounded border border-2 border-medium-grey/25 py-3 px-3 outline-0 hover:border-none hover:ring-2 hover:ring-primary  focus:border-none focus:ring focus:ring-2 focus:ring-primary ${
-          error === undefined
-            ? "focus:ring-primary"
-            : "ring-red-500 ring ring-2"
+        className={`rounded border border-2 border-medium-grey/25 py-2 px-3 outline-0 hover:border-primary focus:border-primary ${
+          error === undefined ? "focus:border-primary" : "border-red-500"
         }`}
       />
-
       <span className="text-red-600 pl-1">{error}</span>
     </div>
   )
 );
+
+Input.displayName = "Input";
