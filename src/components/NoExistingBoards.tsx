0ca99cdeceac4ad2a0371eef1e344a119fa8da98
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-11-10 09:16:14 +0100

    improved layout of empty boards page

--- src/components/NoExistingBoards.tsx
+++ src/components/NoExistingBoards.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export const NoExistingBoards = ({ onAddNewBoard }: Props) => (
-  <div className="flex h-full flex-col items-center justify-center gap-8 dark:bg-very-dark-grey">
+  <div className="m-auto flex h-full max-w-screen-sm flex-col items-center justify-center gap-8 dark:bg-very-dark-grey">
     <p className="heading-l text-medium-grey">
       There is no board existing. Create a new board to get started.
     </p>
