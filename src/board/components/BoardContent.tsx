5009750db4d27542221a0709731bd5fe7bbfc7d6
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-11-24 12:02:11 +0100

    implemented drag and drop for touch devices

--- src/board/components/BoardContent.tsx
+++ src/board/components/BoardContent.tsx
@@ -3,6 +3,13 @@ import { BoardColumnComponent } from "../../boardColumn/components/BoardColumnCo
 import { NoExistingBoards } from "./NoExistingBoards";
 import { useBoards } from "../hooks/useBoards";
 import { useBoardColumns } from "../../boardColumn/hooks/useBoardColumns";
+import { useDragLayer } from "react-dnd";
+import { Task } from "../../task/model/Task";
+import { TaskComponent } from "../../task/components/TaskComponent";
+import { useMobile } from "../../hooks/useMobile";
+
+const TASK_CARD_HEIGHT = 67;
+const HORIZONTAL_SCROLL_PADDING_OFFSET = 16;
 
 interface Props {
   onAddNewBoard: () => void;
@@ -12,12 +19,28 @@ export const BoardContent = ({ onAddNewBoard }: Props) => {
   const { data: boards } = useBoards();
   const { data: boardColumns, isInitialLoading } = useBoardColumns();
 
+  const isMobile = useMobile();
+  const { isDragging, item, offsetY, offsetX } = useDragLayer((monitor) => {
+    return {
+      isDragging: monitor.isDragging(),
+      item: monitor.getItem<Task>(),
+      offsetY:
+        (monitor.getInitialSourceClientOffset()?.y ?? 0) +
+        (monitor.getDifferenceFromInitialOffset()?.y ?? 0) -
+        TASK_CARD_HEIGHT,
+      offsetX:
+        (monitor.getInitialSourceClientOffset()?.x ?? 0) +
+        (monitor.getDifferenceFromInitialOffset()?.x ?? 0) +
+        HORIZONTAL_SCROLL_PADDING_OFFSET,
+    };
+  });
+
   if (boards?.length === 0) {
     return <NoExistingBoards onAddNewBoard={onAddNewBoard} />;
   }
 
   return (
-    <div className="flex h-full justify-items-start gap-6 p-8 dark:bg-very-dark-grey">
+    <div className="relative flex h-full snap-x scroll-pl-4 justify-items-start gap-6 overflow-x-scroll p-8 dark:bg-very-dark-grey">
       {isInitialLoading && (
         <div className="flex w-[250px] flex-col gap-3">
           <div className="my-3 h-4 animate-pulse rounded-full bg-gray-300" />
@@ -30,6 +53,18 @@ export const BoardContent = ({ onAddNewBoard }: Props) => {
       {boardColumns?.map((boardColumn) => (
         <BoardColumnComponent key={boardColumn.id} boardColumn={boardColumn} />
       ))}
+
+      {isMobile && isDragging && (
+        <div
+          className={`pointer-events-none absolute min-w-[250px] max-w-[250px]`}
+          style={{
+            top: offsetY,
+            left: offsetX,
+          }}
+        >
+          <TaskComponent task={item} onClick={() => undefined} />
+        </div>
+      )}
 
       <AddNewColumnForm />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { useMobile } from "./hooks/useMobile";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 const queryClient = new QueryClient();
+
+const DnDProviderWrapper = () => {
+  const isMobile = useMobile();
+  return (
+    <DndProvider
+      backend={isMobile ? TouchBackend : HTML5Backend}
+      options={{ enableMouseEvents: true }}
+    >
+      <App />
+    </DndProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeContextProvider>
         <SelectedBoardContextProvider>
-          <DndProvider backend={HTML5Backend}>
-            <App />
-          </DndProvider>
+          <DnDProviderWrapper />
         </SelectedBoardContextProvider>
       </ThemeContextProvider>
       <ReactQueryDevtools initialIsOpen={false} />
